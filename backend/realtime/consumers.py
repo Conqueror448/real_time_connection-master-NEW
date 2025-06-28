5d8282ad0b984664a186da7d68ccf8c24d26dea2
Conqueror448 <33167910+Conqueror448@users.noreply.github.com>
2025-06-28 15:04:29 -0400

    Make it asynchronous updates`

--- backend/realtime/consumers.py
+++ backend/realtime/consumers.py
@@ -1,10 +1,28 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
+
 class EchoConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        await self.accept()           # handshake OK
+        # 1) put every socket in the same group
+        self.group_name = "echo"
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # 2) tidy-up
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def receive(self, text_data):
-        # send whatever we got right back
-        await self.send(text_data=json.dumps({"echo": text_data}))
+        # 3) broadcast whatever we got
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                "type": "broadcast",      # ↙ handler name below
+                "message": text_data,
+            },
+        )
+
+    # 4) handler that the group dispatcher calls
+    async def broadcast(self, event):
+        await self.send(text_data=json.dumps({"echo": event["message"]}))
